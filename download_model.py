f70b3036a327baa783d7f2cb9d0a6b536a167c29
DaiXilei <xileidai@outlook.com>
2025-03-23 22:39:31 +0800

    sync

--- download_model.py
+++ download_model.py
@@ -1,8 +1,14 @@
 from transformers import AutoModel, AutoTokenizer
 
 model_name = "gpt2"
-cache_dir = "Pretrained_model"
+local_dir = "local_models/gpt2"
 
-# Downloads and caches model/tokenizer locally
-tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
-model = AutoModel.from_pretrained(model_name, cache_dir=cache_dir)
+# # Download and save
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModel.from_pretrained(model_name)
+
+# tokenizer.save_pretrained(local_dir)
+# model.save_pretrained(local_dir)
+
+tokenizer = AutoTokenizer.from_pretrained(local_dir)
+model = AutoModel.from_pretrained(local_dir)
